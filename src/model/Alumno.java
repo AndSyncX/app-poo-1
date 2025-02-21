7c9d6503d3f31b0a952d2dd12f66c13dd563c222
AndSyncX <fernandezp.joe@gmail.com>
2025-02-20 20:17:30 -0500

    Aplicando polimorfismo a las demas clases

--- src/model/Alumno.java
+++ src/model/Alumno.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Alumno extends Persona {
-
+    @Override
+    public boolean registrarAsistencia(String codIdentificador, String fechaAsistencia, String horaAsistencia, Boolean valorAsistencia) {
+        System.out.println("Aplicar lógica " +
+                "para la asistencia " +
+                "del Alumno");
+        return true;
+    }
 }
